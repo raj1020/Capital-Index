b61f083e4398a6f4019c5e1ef44f70b740b404ad
raj1020 <raj.1020@hotmail.com>
2019-10-07 00:03:27 +1100

    added code in the method

--- capitalindex.rb
+++ capitalindex.rb
@@ -2,9 +2,18 @@
 def capital_index(str)
     #Your code here
     
+    str_split = str.split("") #To return an array of individual letters of the string
+    newar =  []
+    for i in str_split
+       if i === i.capitalize && i
+            newar.push(str_split.index(i))
+       end
+
+    end
     
+    return newar
 end
 
-capital_index("rABbiT")
-capital_index("lowercase")
-capital_index("!@#HI!")
+p capital_index("rABbiT")
+p capital_index("lowercase")
+p capital_index("!@#HI!")
