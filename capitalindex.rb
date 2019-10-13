885a41c7fc4771283fc3e1dc3201fa677587cd92
raj1020 <raj.1020@hotmail.com>
2019-10-14 10:15:51 +1100

    added comments and questions at the top

--- capitalindex.rb
+++ capitalindex.rb
@@ -1,22 +1,33 @@
+#Create a method that will take a single string as an argument, and will return an ordered 
+#array containing the index of all capital letters. Return an empty array if no capitals 
+#are found. There may be some special characters included.
+
+#E.g. capital_index("rABbiT"), should return [1, 2, 5]
+#E.g. capital_index("lowercase"), should return [ ]
+#E.g. capital_index("!@#HI!"), should return [3, 4]
+
 
 def capital_index(str)
     #Your code here
     
     str_split = str.split("") #To return an array of individual letters of the string
-    newar =  []
+    new_array =  []
     
-    charsu=("A".."Z").to_a
+    letter_array=("A".."Z").to_a
 
     for i in str_split
-       if charsu.include?(i)
-            newar.push(str_split.index(i))
+       
+        if letter_array.include?(i)
+            new_array.push(str_split.index(i))
+            i.downcase! #to convert the letter already pushed in new_array into lower case
        end
 
     end
     
-    return newar
+    return new_array
 end
 
 p capital_index("rABbiT")
 p capital_index("lowercase")
 p capital_index("!@#HI!")
+p capital_index("WOW! Super ENTHUSIASTIC!!")
